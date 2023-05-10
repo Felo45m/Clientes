494e1ab49804849f8e3df69fc85fdbca6424a6bd
Felo <felipemelo713@gmail.com>
2023-05-10 14:57:34 -0300

    Creating client with POST and getting data with GET

--- src/main/java/com/felo/clientes/config/TestConfig.java
+++ src/main/java/com/felo/clientes/config/TestConfig.java
@@ -1,14 +1,10 @@
 package com.felo.clientes.config;
 
-import java.time.LocalDate;
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-import com.felo.clientes.model.entities.Client;
 import com.felo.clientes.model.repositories.ClientRepository;
 
 @Configuration
@@ -21,10 +17,6 @@ public class TestConfig implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		Client u1 = new Client(null, "Maria Brown", "00000000000", LocalDate.now());
-		
-		clientRepository.saveAll(Arrays.asList(u1));
-		
 	}
 	
 }
--- src/main/java/com/felo/clientes/rest/ClientController.java
+++ src/main/java/com/felo/clientes/rest/ClientController.java
@@ -2,11 +2,14 @@ package com.felo.clientes.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.felo.clientes.model.entities.Client;
 import com.felo.clientes.model.repositories.ClientRepository;
@@ -26,6 +29,11 @@ public class ClientController {
 	@ResponseStatus(HttpStatus.CREATED)
 	public Client saveClient(@RequestBody Client client) {
 		return repository.save(client);
+	}
+	
+	@GetMapping("{id}")
+	public Client findById(@PathVariable Integer id) {
+		return repository.findById(id).orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 	}
 
 }
